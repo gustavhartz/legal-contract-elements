68d4a6136f586a342037e2be16d78d4d2a831ee9
Gustav Hartz <gustav.s.hartz@gmail.com>
2021-12-02 20:29:39 +0100

    Cleanup

--- src/models/drqa/drqa.py
+++ src/models/drqa/drqa.py
@@ -24,42 +24,25 @@ class AlignQuestionEmbedding(nn.Module):
         self.relu = nn.ReLU()
 
     def forward(self, context, question, question_mask):
-
-        # context = [bs, ctx_len, emb_dim]
-        # question = [bs, qtn_len, emb_dim]
-        # question_mask = [bs, qtn_len]
-
         ctx_ = self.linear(context)
         ctx_ = self.relu(ctx_)
-        # ctx_ = [bs, ctx_len, emb_dim]
-
         qtn_ = self.linear(question)
         qtn_ = self.relu(qtn_)
-        # qtn_ = [bs, qtn_len, emb_dim]
 
         qtn_transpose = qtn_.permute(0, 2, 1)
-        # qtn_transpose = [bs, emb_dim, qtn_len]
 
         align_scores = torch.bmm(ctx_, qtn_transpose)
-        # align_scores = [bs, ctx_len, qtn_len]
 
         qtn_mask = question_mask.unsqueeze(1).expand(align_scores.size())
-        # qtn_mask = [bs, 1, qtn_len] => [bs, ctx_len, qtn_len]
 
-        # Fills elements of self tensor(align_scores) with value(-float(inf)) where mask is True. 
-        # The shape of mask must be broadcastable with the shape of the underlying tensor.
         align_scores = align_scores.masked_fill(qtn_mask == 1, -float('inf'))
-        # align_scores = [bs, ctx_len, qtn_len]
 
         align_scores_flat = align_scores.view(-1, question.size(1))
-        # align_scores = [bs*ctx_len, qtn_len]
 
         alpha = F.softmax(align_scores_flat, dim=1)
         alpha = alpha.view(-1, context.shape[1], question.shape[1])
-        # alpha = [bs, ctx_len, qtn_len]
 
         align_embedding = torch.bmm(alpha, question)
-        # align = [bs, ctx_len, emb_dim]
 
         return align_embedding
 
@@ -84,7 +67,6 @@ class StackedBiLSTM(nn.Module):
                                       batch_first=True, bidirectional=True))
 
     def forward(self, x):
-        # x = [bs, seq_len, feature_dim]
 
         outputs = [x]
         for i in range(self.num_layers):
@@ -96,7 +78,6 @@ class StackedBiLSTM(nn.Module):
             outputs.append(lstm_out)
 
         output = torch.cat(outputs[1:], dim=2)
-        # [bs, seq_len, num_layers*num_dir*hidden_dim]
 
         output = F.dropout(output, p=self.dropout)
 
@@ -113,35 +94,24 @@ class LinearAttentionLayer(nn.Module):
 
     def forward(self, question, question_mask):
 
-        # question = [bs, qtn_len, input_dim] = [bs, qtn_len, bi_lstm_hid_dim]
-        # question_mask = [bs,  qtn_len]
-
         qtn = question.view(-1, question.shape[-1])
-        # qtn = [bs*qtn_len, hid_dim]
 
         attn_scores = self.linear(qtn)
-        # attn_scores = [bs*qtn_len, 1]
 
         attn_scores = attn_scores.view(question.shape[0], question.shape[1])
-        # attn_scores = [bs, qtn_len]
 
         attn_scores = attn_scores.masked_fill(question_mask == 1, -float('inf'))
 
         alpha = F.softmax(attn_scores, dim=1)
-        # alpha = [bs, qtn_len]
 
         return alpha
 
 
 def weighted_average(x, weights):
-    # x = [bs, len, dim]
-    # weights = [bs, len]
 
     weights = weights.unsqueeze(1)
-    # weights = [bs, 1, len]
 
     w = weights.bmm(x).squeeze(1)
-    # w = [bs, 1, dim] => [bs, dim]
 
     return w
 
@@ -156,26 +126,15 @@ class BilinearAttentionLayer(nn.Module):
 
     def forward(self, context, question, context_mask):
 
-        # context = [bs, ctx_len, ctx_hid_dim] = [bs, ctx_len, hid_dim*6] = [bs, ctx_len, 768]
-        # question = [bs, qtn_hid_dim] = [bs, qtn_len, 768]
-        # context_mask = [bs, ctx_len]
-
         qtn_proj = self.linear(question)
-        # qtn_proj = [bs, ctx_hid_dim]
 
         qtn_proj = qtn_proj.unsqueeze(2)
-        # qtn_proj = [bs, ctx_hid_dim, 1]
 
         scores = context.bmm(qtn_proj)
-        # scores = [bs, ctx_len, 1]
 
         scores = scores.squeeze(2)
-        # scores = [bs, ctx_len]
 
         scores = scores.masked_fill(context_mask == 1, -float('inf'))
-
-        # alpha = F.log_softmax(scores, dim=1)
-        # alpha = [bs, ctx_len]
 
         return scores
 
@@ -187,8 +146,6 @@ class DocumentReader(nn.Module):
         super().__init__()
 
         self.device_ = device
-
-        # self.embedding = self.get_glove_embedding()
 
         self.context_bilstm = StackedBiLSTM(embedding_dim * 2, hidden_dim, num_layers, dropout)
 
@@ -224,43 +181,28 @@ class DocumentReader(nn.Module):
 
     def forward(self, context, question, context_mask, question_mask):
 
-        # context = [bs, len_c]
-        # question = [bs, len_q]
-        # context_mask = [bs, len_c]
-        # question_mask = [bs, len_q]
-
         ctx_embed = self.glove_embedding(context)
-        # ctx_embed = [bs, len_c, emb_dim]
 
         ques_embed = self.glove_embedding(question)
-        # ques_embed = [bs, len_q, emb_dim]
 
         ctx_embed = self.dropout(ctx_embed)
 
         ques_embed = self.dropout(ques_embed)
 
         align_embed = self.align_embedding(ctx_embed, ques_embed, question_mask)
-        # align_embed = [bs, len_c, emb_dim]  
 
         ctx_bilstm_input = torch.cat([ctx_embed, align_embed], dim=2)
-        # ctx_bilstm_input = [bs, len_c, emb_dim*2]
 
         ctx_outputs = self.context_bilstm(ctx_bilstm_input)
-        # ctx_outputs = [bs, len_c, hid_dim*layers*dir] = [bs, len_c, hid_dim*6]
 
         qtn_outputs = self.question_bilstm(ques_embed)
-        # qtn_outputs = [bs, len_q, hid_dim*6]
 
         qtn_weights = self.linear_attn_question(qtn_outputs, question_mask)
-        # qtn_weights = [bs, len_q]
 
         qtn_weighted = weighted_average(qtn_outputs, qtn_weights)
-        # qtn_weighted = [bs, hid_dim*6]
 
         start_scores = self.bilinear_attn_start(ctx_outputs, qtn_weighted, context_mask)
-        # start_scores = [bs, len_c]
 
         end_scores = self.bilinear_attn_end(ctx_outputs, qtn_weighted, context_mask)
-        # end_scores = [bs, len_c]
 
         return start_scores, end_scores
